f8992a37ef9a341ee4dae2ac98598a9f11a3d0d2
yangfeng <yangfeng>
2019-11-29 12:08:31 +0800

    xxx

--- src/markets/huobi_future.py
+++ src/markets/huobi_future.py
@@ -11,12 +11,16 @@ Email:  huangtao@ifclover.com
 
 import gzip
 import json
+import asyncio
+from quant import const
 
 from quant.utils import logger
 from quant.utils.web import Websocket
 from quant.const import MARKET_TYPE_KLINE
 from quant.order import ORDER_ACTION_BUY, ORDER_ACTION_SELL
 from quant.event import EventTrade, EventKline, EventOrderbook
+from quant.platform.huobi_future import HuobiFutureRestAPI
+from quant.heartbeat import heartbeat
 
 
 class HuobiFutureMarket:
@@ -34,15 +38,21 @@ class HuobiFutureMarket:
     def __init__(self, **kwargs):
         self._platform = kwargs["platform"]
         self._wss = kwargs.get("wss", "wss://www.hbdm.com")
+        self._host = kwargs.get("host", "https://api.hbdm.com")
         self._symbols = list(set(kwargs.get("symbols")))
         self._channels = kwargs.get("channels")
         self._orderbook_length = kwargs.get("orderbook_length", 10)
+        self._access_key = kwargs.get("access_key", "")
+        self._secret_key = kwargs.get("secret_key", "")
 
         self._c_to_s = {}  # {"channel": "symbol"}
 
         url = self._wss + "/ws"
         self._ws = Websocket(url, self.connected_callback, process_binary_callback=self.process_binary)
         self._ws.initialize()
+
+        self._rest_api = HuobiFutureRestAPI(self._host, self._access_key, self._secret_key)
+        self._initialize()
 
     async def connected_callback(self):
         """ After create connection to Websocket server successfully, we will subscribe orderbook event.
@@ -75,8 +85,6 @@ class HuobiFutureMarket:
                         "sub": channel
                     }
                     await self._ws.send(data)
-            else:
-                logger.error("channel error! channel:", ch, caller=self)
 
     async def process_binary(self, msg):
         """ Process binary message that received from Websocket connection.
@@ -154,3 +162,69 @@ class HuobiFutureMarket:
             return None
         self._c_to_s[channel] = symbol
         return channel
+
+    def _initialize(self):
+        """ Initialize."""
+        for channel in self._channels:
+            if channel == const.MARKET_TYPE_KLINE_5M:
+                heartbeat.register(self.create_kline_tasks, 1, const.MARKET_TYPE_KLINE_5M)
+            elif channel == const.MARKET_TYPE_KLINE_15M:
+                heartbeat.register(self.create_kline_tasks, 1, const.MARKET_TYPE_KLINE_15M)
+            elif channel == const.MARKET_TYPE_KLINE_1H:
+                heartbeat.register(self.create_kline_tasks, 1, const.MARKET_TYPE_KLINE_1H)
+            # else:
+            #     logger.error("channel error! channel:", channel, caller=self)
+
+    async def create_kline_tasks(self, kline_type, *args, **kwargs):
+        """ Create some tasks to fetch kline information
+
+        Args:
+            kline_type: Type of line, kline or kline_5m or kline_15m
+
+        NOTE: Because of REST API request limit, we only send one request pre minute.
+        """
+        for index, symbol in enumerate(self._symbols):
+            asyncio.get_event_loop().call_later(index, self.delay_kline_update, symbol, kline_type)
+
+    def delay_kline_update(self, symbol, kline_type):
+        """ Do kline update.
+        """
+        asyncio.get_event_loop().create_task(self.do_kline_update(symbol, kline_type))
+
+    async def do_kline_update(self, symbol, kline_type):
+        if kline_type == const.MARKET_TYPE_KLINE_5M:
+            range_type = "5min"
+        elif kline_type == const.MARKET_TYPE_KLINE_15M:
+            range_type = "15min"
+        elif kline_type == const.MARKET_TYPE_KLINE_1H:
+            range_type = "60min"
+        elif kline_type == const.MARKET_TYPE_KLINE_1H:
+            range_type = "4hour"
+        elif kline_type == const.MARKET_TYPE_KLINE_1H:
+            range_type = "1day"
+        else:
+            return
+        result, error = await self._rest_api.get_kline(symbol, size =200, period = range_type)
+
+        if error:
+            return
+
+        open, hig, low, close = [], [], [], []
+        if len(result):
+            for item in result["data"]:
+                open.append(item["open"])
+                hig.append(item["high"])
+                low.append(item["low"])
+                close.append(item["close"])
+
+        kline = {
+            "platform": self._platform,
+            "symbol": symbol,
+            "open": open,#最近的数据在最后面
+            "high": hig,
+            "low": low,
+            "close": close,
+            "kline_type": kline_type
+        }
+        EventKline(**kline).publish()
+        logger.info("symbol:", symbol, "kline:", kline, caller=self)
